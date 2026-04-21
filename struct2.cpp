6c6377b8a5ec17c7d1cac5681ea8470f5c1026e0
BimoFr <bimofadhlurrahman@gmail.com>
2026-04-21 09:42:01 +0700

    menambahkan implementasi struct Mahasiswa dan fungsi main untuk input/output data

---
+++ struct2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string NIM;
+    string nama;
+    string alamat;
+    int umur;
+};
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa: ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.umur;
+    
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.NIM;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
+}

0e3321b5a16ff6913727696853f7e10ab8f73f96
BimoFr <bimofadhlurrahman@gmail.com>
2026-04-21 10:13:22 +0700

    Menambahkan struct Mahasiswa dan DetailAlamat

---
+++ struct4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string NIM;
+    string nama;
+    DetailAlamat alamat;
+    int umur;  
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    getline(cin, mhs.NIM);
+    cout << "Nama Mahasiswa: ";
+    getline(cin, mhs.nama);
+    cout << "Alamat Mahasiswa: " << endl;
+    cout << "\t Nama Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.umur;
+    
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.NIM;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : ";
+    cout << "\nDesa : " << mhs.alamat.desa;
+    cout << "\nKota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
+
+    return 0;
+}
